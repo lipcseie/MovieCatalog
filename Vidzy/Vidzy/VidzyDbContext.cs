363b75ef450c46b76ff7b040ce53783c74471bc0
lipcseie <lipcsei.e@gmail.com>
2024-03-09 16:19:49 +0100

    add fixed connection string

--- Vidzy/Vidzy/VidzyDbContext.cs
+++ Vidzy/Vidzy/VidzyDbContext.cs
@@ -3,15 +3,16 @@ using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace Vidzy
 {
-    public class VidzyDbContext: DbContext
+    public class VidzyDbContext : DbContext
     {
-      public DbSet<Video> Videos { get; set; }
+        public DbSet<Video> Videos { get; set; }
 
-      public DbSet<Genre> Genres { get; set; }
+        public DbSet<Genre> Genres { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\DESKTOP-I445CNP\\SQLEXPRESS;Database=Vidzy;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-I445CNP\\SQLEXPRESS;Database=Vidzy;Trusted_Connection=True;TrustServerCertificate=True;");
+
         }
     }
 }
