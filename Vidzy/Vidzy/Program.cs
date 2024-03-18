b42f01ace95e41a80c9d4b05affc37148028aa44
lipcseie <lipcsei.e@gmail.com>
2024-03-18 17:46:06 +0100

    add data to the dtabase

--- Vidzy/Vidzy/Program.cs
+++ Vidzy/Vidzy/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var context = new VidzyDbContext())
+            {
+                var actionGenre = new Genre { Id = 3, Name = "Action2" };
+                var fantasyGenre = new Genre { Id = 4, Name = "Fantasy2" };
+
+                var LOTR = new Video { Name = "LOTR", ReleaseDate = new DateTime(2002, 01, 15), Genres = fantasyGenre };
+                var Matrix = new Video { Name = "Matric", ReleaseDate = new DateTime(1999, 02, 02), Genres = actionGenre };
+                var LOTR2 = new Video { Name = "LOTR2", ReleaseDate = new DateTime(2004, 06, 12), Genres = fantasyGenre };
+
+                context.Genres.AddRange(actionGenre, fantasyGenre);
+                context.Videos.AddRange(LOTR, Matrix, LOTR2);
+
+                context.SaveChanges();
+
+
+
+            }
         }
     }
 }
